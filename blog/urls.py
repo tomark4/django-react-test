290b5301bd49cd0ff60ba1876f3272ff7a5322fd
Jose Quintero <jg.fernandez31@gmail.com>
2020-11-11 07:40:41 -0400

    prueba con django json response

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, PruebaView
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('prueba/',PruebaView.as_view(), name='prueba')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.http import JsonResponse
 # Create your views here.
 
 class IndexView(TemplateView):
     template_name = 'blog/index.html'
+
+class PruebaView(TemplateView):
+    #template_name = 'blog/index.html'
+
+    def get(self, request, *args, **kwargs):
+        return JsonResponse({'id':'1', 'name':'jose'})
