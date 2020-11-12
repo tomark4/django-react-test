2b30e695432f3d731684a033ce02883966f41e0b
Jose Quintero <jg.fernandez31@gmail.com>
2020-11-12 16:29:14 -0400

    update model

--- prueba/models.py
+++ prueba/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+# comment in model
 # Create your models here.
 class Categoria(models.Model):
     name = models.CharField(max_length=100)
